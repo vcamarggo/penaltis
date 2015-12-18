86af75a6b55c964cbaf0a099b8be08d7aaab4475
vcamarggo <vinicamarggo@hotmail.com>
2015-12-18 19:13:57 -0200

    classes base

---
+++ src/br/uem/enumeration/Ponto.java
@@ -0,0 +1,11 @@
+package br.uem.enumeration;
+
+/**
+ * @author V.Camargo
+ * 
+ * @Date 18/12/2015
+ */
+
+public enum Ponto {
+
+}
---
+++ src/br/uem/model/Torcida.java
@@ -0,0 +1,11 @@
+package br.uem.model;
+
+/**
+ * @author V.Camargo
+ * 
+ * @Date 18/12/2015
+ */
+
+public class Torcida {
+
+}
